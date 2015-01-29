dcbeec1653dd7db25329c0154e4b5c3681a427f0
glcoakley <glc@cbcc.cc>
2015-01-28 22:36:16 -0500

    Hour 7 example

---
+++ FieldButtonFun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FieldButtonFun
+//
+//  Created by Gary Coakley on 1/28/15.
+//  Copyright (c) 2015 Gary Coakley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FieldButtonFun/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FieldButtonFun
+//
+//  Created by Gary Coakley on 1/28/15.
+//  Copyright (c) 2015 Gary Coakley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
